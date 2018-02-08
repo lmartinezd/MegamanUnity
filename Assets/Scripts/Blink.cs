05525fd7627daf64172db40811fa8626be2d5c07
logonrm <logonrm@fiap.com.br>
2018-02-07 22:50:03 -0200

    aula jump and run animation

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Blink : MonoBehaviour {
+
+	public float intervalo;
+	// Use this for initialization
+	IEnumerator Start () {
+		GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(intervalo);
+        GetComponent<SpriteRenderer>().enabled = true;
+        yield return new WaitForSeconds(intervalo);
+
+        StartCoroutine(Start());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("game-scene");
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float velocidade;
+	public float impulso;
+	Rigidbody2D rb;
+
+	SpriteRenderer spriteRender;
+
+	public Transform chaoVerificar;
+	bool estadoChao;
+
+	// Use this for initialization
+	void Start () {
+		spriteRender = GetComponent<SpriteRenderer> ();
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move_x = Input.GetAxisRaw ("Horizontal") * velocidade * Time.deltaTime;
+		transform.Translate (move_x, 0.0f, 0.0f);
+
+		estadoChao = Physics2D.Linecast (transform.position,
+			chaoVerificar.position,1 << LayerMask.NameToLayer("Piso"));
+		print (estadoChao);
+
+		if (Input.GetButtonDown ("Jump") && (estadoChao)) {
+			rb.velocity = new Vector2 (0.0f, impulso);
+		}
+
+		if (move_x > 0) {
+			spriteRender.flipX = false;
+		}else if(move_x < 0) {
+			spriteRender.flipX = true;
+		}
+	}
+ 
+}
